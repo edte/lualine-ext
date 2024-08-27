5cb3020528b69c5f87baa514c754610e9a2515f1
orzorzzhang <orzorzzhang@tencent.com>
2024-08-27 21:03:21 +0800

    add project name

--- lua/lualine/components/project-name.lua
+++ lua/lualine/components/project-name.lua
@@ -2,56 +2,67 @@ local CProjectName = require("lualine.component"):extend()
 
 -- 获取tmux窗口名称
 local function getTmuxWindowName()
-  local res = "nil"
-  res = vim.fn.system({ "tmux", "display-message", "-p", "#W" })
-  if res == nil or res == "" then
-    return res
-  end
-  res = res:gsub("%c", "") -- 删除所有控制字符
-  if res == nil or res == "" then
-    return res
-  end
+	local res = "nil"
+	res = vim.fn.system({ "tmux", "display-message", "-p", "#W" })
+	if res == nil or res == "" then
+		return res
+	end
+	res = res:gsub("%c", "") -- 删除所有控制字符
+	if res == nil or res == "" then
+		return res
+	end
 
-  if res == "failed to connect to server" then
-    res = ""
-  end
-  if res == "reattach-to-user-namespace" then
-    res = ""
-  end
+	if res == "failed to connect to server" then
+		res = ""
+	end
+	if res == "reattach-to-user-namespace" then
+		res = ""
+	end
 
-  return res
+	return res
+end
+
+function IsGitProject()
+	return vim.fn.system([[git rev-parse --show-toplevel 2> /dev/null]]) ~= ""
 end
 
 -- 获取git远程仓库名称
 local function getGitRemoteProjectName()
-  return vim.fn.system([[git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p']])
+	return vim.fn.system([[git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p']])
 end
 
 -- 获取git本地仓库名称
 local function getGitLocalProjectName()
-  return vim.fn.system([[ TOP=$(git rev-parse --show-toplevel); echo ${TOP##*/} ]])
+	return vim.fn.system([[ TOP=$(git rev-parse --show-toplevel); echo ${TOP##*/} ]])
 end
 
 CProjectName.init = function(self, options)
-  CProjectName.super.init(self, options)
+	CProjectName.super.init(self, options)
 end
 
 CProjectName.update_status = function(self)
-  local res = ""
-  res = getTmuxWindowName()
-  if res == "" then
-    res = getGitRemoteProjectName()
-  end
-  if res == "" then
-    res = getGitLocalProjectName()
-  end
+	local res = getTmuxWindowName()
+	res = res:gsub("%c", "") -- 删除所有控制字符
+	if res ~= "" then
+		return res
+	end
 
-  res = res:gsub("%c", "") -- 删除所有控制字符
-  if res == nil or res == "" then
-    return res
-  end
+	if IsGitProject() == false then
+		res = vim.fn.system("basename $(pwd)")
+		res = res:gsub("%c", "") -- 删除所有控制字符
+		return res
+	end
 
-  return res
+	res = getGitRemoteProjectName()
+	res = res:gsub("%c", "") -- 删除所有控制字符
+	if res ~= "" then
+		return res
+	end
+
+	res = getGitLocalProjectName()
+	res = res:gsub("%c", "") -- 删除所有控制字符
+
+	return res
 end
 
 return CProjectName
